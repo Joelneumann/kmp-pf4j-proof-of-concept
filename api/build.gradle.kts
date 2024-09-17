61fac32472be9129bdf997c9e4fd5569c19e3919
joelneumann <joel.neumann01@gmail.com>
2024-09-17 12:15:30 +0200

    works with publishToMavenLocal

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -25,19 +25,13 @@ kotlin {
     jvmToolchain(17)
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))  // Set Java version to 17
-    }
-}
-
 afterEvaluate {
     publishing {
         publications {
             // Creates a Maven publication called "release".
             create<MavenPublication>("release") {
                 from(components["java"])
-                groupId = "de.Joelneumann.kmp-pf4j-proof-of-concept"
+                groupId = "de.joelneumann.kmp-pf4j-proof-of-concept"
                 artifactId = "api"
                 version = "1.0.0"
             }
