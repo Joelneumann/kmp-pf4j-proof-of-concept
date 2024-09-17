bb4916479c44e7443701ae6979b1c059b234ad1d
joelneumann <joel.neumann01@gmail.com>
2024-09-17 13:45:14 +0200

    changed publishing

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("maven-publish")
 }
 
-group = "de.joelneumann"
+group = "de.joelneumann.kmp-pf4j-proof-of-concept"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -25,16 +25,11 @@ kotlin {
     jvmToolchain(17)
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            // Creates a Maven publication called "release".
-            create<MavenPublication>("release") {
-                from(components["java"])
-                groupId = "de.joelneumann.kmp-pf4j-proof-of-concept"
-                artifactId = "api"
-                version = "1.0.0"
-            }
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+            artifactId = "api"
         }
     }
 }
